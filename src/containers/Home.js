02defd8ce6938a5e077fcf2a77cb19681317ffbc
Tam Pham <tampham47@live.com>
2018-05-20 21:14:09 +0700

    d

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { withSiteData } from 'react-static';
+import { withRouteData } from 'react-static';
 
-export default withSiteData(() => {
+export default withRouteData(({ postList }) => {
   return (
     <div>
       <section className="container">
-        <h1>Xaolonist (Phạm Minh Tâm)</h1>
-        <h4>Who is a scientist, an anthropologist, a psychologist, a moralist, an alchemist...</h4>
-        <h4>FE leader at Quoine | Saigon, Viet nam.</h4>
+        <h1>d. xaolonist</h1>
+        <h4>
+          who is a scientist, an anthropologist, a psychologist,
+          a moralist, an&nbsp;alchemist...
+        </h4>
+        <h4>lead of front-end at quoine | saigon, viet nam.</h4>
+        <ul>
+          <li className="info">
+            <span role="img" aria-label="pineapple">🍍</span>
+            <a href="https://www.linkedin.com/in/tam-pham-51b41192/" target="_blank" rel="noopener noreferrer">linkedin</a>
+            <span>, </span>
+            <a href="https://medium.com/@xaolonist" target="_blank" rel="noopener noreferrer">medium</a>
+            <span>, </span>
+            <a href="https://github.com/tampham47" target="_blank" rel="noopener noreferrer">github</a>
+            <span>, </span>
+            <a href="https://codepen.io/tampham47" target="_blank" rel="noopener noreferrer">codepen</a>
+            <span>, </span>
+          </li>
+          <li>
+            <span role="img" aria-label="mail">💌</span>
+            <a href="mailto:tampham47@live.com">tampham47@live.com</a>
+          </li>
+          <li>
+            <span role="img" aria-label="phone">🤙🏿</span>
+            <a href="tel:01643652922">01643-652-922</a>
+          </li>
+        </ul>
       </section>
       <section className="container">
-        <h1>Articles</h1>
+        <h1>medium của tớ,</h1>
+        <ul className="medium-list">
+          {postList.map(post => {
+            return (
+              <li key={post.id} className="medium-item">
+                <a href={post.link} target="_blank" rel="noopener noreferrer" className="medium-title">{post.title}</a>
+                <p className="medium-desc">{post.desc}</p>
+              </li>
+            );
+          })}
+        </ul>
       </section>
+      <footer className="container">
+        <a href="">created by react-static</a>
+      </footer>
     </div>
   );
 });
