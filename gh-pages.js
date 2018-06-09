e7c24dbc03a459cae0f96964489429634a7cba93
Tam Pham <tampham47@live.com>
2018-06-09 16:47:08 +0700

    Add codebuild (#6)
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d

--- gh-pages.js
+++ gh-pages.js
@@ -3,6 +3,11 @@ import ghpages from 'gh-pages';
 /* eslint-disable */
 ghpages.publish('dist', {
   branch: 'master',
+  repo: 'https://'+ process.env.GITHUB_AUTH +'@github.com/tampham47/tampham47.github.io.git',
+  user: {
+    name: 'Xaolonist',
+    email: 'tampham47@live.com',
+  },
 }, (err) => {
   if (!err) {
     console.log('Xalonist\'s landing page was deployed successful.');
