8fdc4a7d391638d85afd68383cfd9fe1380277f2
Tam Pham <tampham47@live.com>
2018-06-09 15:30:28 +0700

    d

--- gh-pages.js
+++ gh-pages.js
@@ -3,6 +3,10 @@ import ghpages from 'gh-pages';
 /* eslint-disable */
 ghpages.publish('dist', {
   branch: 'master',
+  user: {
+    name: 'Xaolonist',
+    email: 'tampham47@live.com',
+  },
 }, (err) => {
   if (!err) {
     console.log('Xalonist\'s landing page was deployed successful.');
