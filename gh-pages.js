03b65ab406d959b532eaea7e5810ff6375eb1130
Tam Pham <tampham47@live.com>
2018-06-09 16:01:56 +0700

    d

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ import ghpages from 'gh-pages';
 /* eslint-disable */
 ghpages.publish('dist', {
   branch: 'master',
-  repo: 'https://042861227e38cc42e91b87368a77fd2502a39a2f@github.com/tampham47/tampham47.github.io.git',
+  repo: 'https://$token:042861227e38cc42e91b87368a77fd2502a39a2f@github.com/tampham47/tampham47.github.io.git',
   user: {
     name: 'Xaolonist',
     email: 'tampham47@live.com',
