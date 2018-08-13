72a13b71c901c8a2da91195e7b8a00e0e06aa5a3
Tam Pham <tampham47@live.com>
2018-08-13 23:29:36 +0700

    add fav icons (#8)
    * add fav icons
    
    * update new icons & increase server to 7.0.0

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,14 +10,33 @@ const Home = ({ postList }) => {
           <Head>
             <title>d. xaolonist</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta name="description" content="Pham Minh Tam - Lead of front-end at Quoine - Saigon, Vietnam." />
+            <meta name="description" content="Pham Minh Tam - Head of front-end at Quoine - Saigon, Vietnam." />
             <meta name="keywords" content="Frontend Lead, Senior, Sai Gon, Viet Nam" />
             <meta name="author" content="Pham Minh Tam" />
 
             <meta property="og:title" content="d. xaolonist" />
             <meta property="og:description" content="who is a scientist, an anthropologist, a psychologist,
               a moralist, an alchemist..." />
-            <meta property="og:image" content="/xaolonist.png" />
+            <meta property="og:image" content="/apple-icon-180x180.png" />
+
+            <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+            <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+            <meta name="theme-color" content="#ffffff" />
           </Head>
 
           <section className="container">
@@ -26,7 +45,7 @@ const Home = ({ postList }) => {
               who is a scientist, an anthropologist, a psychologist,
               a moralist, an&nbsp;alchemist...
             </h4>
-            <h4>lead of front-end at quoine | saigon, viet nam.</h4>
+            <h4>head of front-end at quoine | saigon, viet nam.</h4>
             <ul>
               <li className="info">
                 <span role="img" aria-label="pineapple">🍍</span>
